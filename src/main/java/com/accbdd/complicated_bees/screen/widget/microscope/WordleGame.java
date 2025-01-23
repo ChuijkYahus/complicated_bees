6c104e324632f54ac9de6c37a34ec4e3d36e4a4a
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-01-23 11:22:27 -0800

    refactor BaseMicroscopeGame

--- src/main/java/com/accbdd/complicated_bees/screen/widget/microscope/WordleGame.java
+++ src/main/java/com/accbdd/complicated_bees/screen/widget/microscope/WordleGame.java
@@ -1,12 +1,21 @@
 package com.accbdd.complicated_bees.screen.widget.microscope;
 
 import com.accbdd.complicated_bees.network.packet.MicroscopeGamePacketClientbound;
+import com.accbdd.complicated_bees.screen.MicroscopeScreen;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 
 public class WordleGame extends AbstractMicroscopeGame {
-    public WordleGame(int pX, int pY, int pWidth, int pHeight) {
-        super(pX, pY, pWidth, pHeight);
+    /**
+     * @param pX         the x of the widget
+     * @param pY         the y of the widget
+     * @param pWidth     the width of the widget
+     * @param pHeight    the height of the widget
+     * @param difficulty the difficulty of the widget (3-10)
+     * @param screen     the screen this widget is attached to
+     */
+    public WordleGame(int pX, int pY, int pWidth, int pHeight, int difficulty, MicroscopeScreen screen) {
+        super(pX, pY, pWidth, pHeight, difficulty, screen);
     }
 
     @Override
