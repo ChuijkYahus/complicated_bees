b5f4fac42615881a162d4aa0251533d899c0069a
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-10-24 15:56:57 -0700

    code cleanup, transition to using native EmiIngredient class instead of custom implementation

--- src/main/java/com/accbdd/complicated_bees/compat/emi/recipe/MutationEmiRecipe.java
+++ src/main/java/com/accbdd/complicated_bees/compat/emi/recipe/MutationEmiRecipe.java
@@ -15,22 +15,21 @@ import dev.emi.emi.api.widget.TextWidget;
 import dev.emi.emi.api.widget.WidgetHolder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static com.accbdd.complicated_bees.ComplicatedBees.MODID;
 
 public class MutationEmiRecipe implements EmiRecipe {
     private final Mutation mutation;
     private final ResourceLocation id;
-    private final List<EmiIngredient> first;
-    private final List<EmiIngredient> second;
-    private final List<EmiStack> result;
-    private final List<EmiStack> extraResults;
-    private final List<EmiIngredient> catalysts;
+    private final EmiIngredient first;
+    private final EmiIngredient second;
+    private final EmiIngredient result;
+    private final EmiIngredient lookups;
 
     public MutationEmiRecipe(Mutation mutation) {
         this.mutation = mutation;
@@ -43,24 +42,23 @@ public class MutationEmiRecipe implements EmiRecipe {
                         "/result/" +
                         mutation.getResult().toString().replace(":", "/")
         );
-        first = List.of(
-                EmiStack.of(mutation.getFirstSpecies().toStack(ItemsRegistration.DRONE.get())),
-                EmiStack.of(mutation.getFirstSpecies().toStack(ItemsRegistration.PRINCESS.get()))
-        );
-        second = List.of(
-                EmiStack.of(mutation.getSecondSpecies().toStack(ItemsRegistration.PRINCESS.get())),
-                EmiStack.of(mutation.getSecondSpecies().toStack(ItemsRegistration.DRONE.get()))
-        );
-        result = List.of(
-                EmiStack.of(mutation.getResultSpecies().toStack(ItemsRegistration.DRONE.get())).setChance(mutation.getChance()),
-                EmiStack.of(mutation.getResultSpecies().toStack(ItemsRegistration.PRINCESS.get())).setChance(mutation.getChance())
-        );
-        extraResults = new ArrayList<>(List.of(result.get(0)));
-        extraResults.add(EmiStack.of(mutation.getResultSpecies().toStack(ItemsRegistration.PRINCESS.get())));
-        extraResults.add(EmiStack.of(mutation.getResultSpecies().toStack(ItemsRegistration.QUEEN.get())));
-        catalysts = new ArrayList<>();
-        catalysts.addAll(mutation.getFirstSpecies().toMembers().stream().map(EmiStack::of).toList());
-        catalysts.addAll(mutation.getSecondSpecies().toMembers().stream().map(EmiStack::of).toList());
+        first = EmiIngredient.of(Ingredient.of(
+                mutation.getFirstSpecies().toStack(ItemsRegistration.DRONE.get()),
+                mutation.getFirstSpecies().toStack(ItemsRegistration.PRINCESS.get())
+        ));
+        second = EmiIngredient.of(Ingredient.of(
+                mutation.getSecondSpecies().toStack(ItemsRegistration.PRINCESS.get()),
+                mutation.getSecondSpecies().toStack(ItemsRegistration.DRONE.get())
+        ));
+        result = EmiIngredient.of(Ingredient.of(
+                mutation.getResultSpecies().toStack(ItemsRegistration.DRONE.get()),
+                mutation.getResultSpecies().toStack(ItemsRegistration.PRINCESS.get())
+        ));
+        lookups = EmiIngredient.of(Ingredient.of(
+                mutation.getFirstSpecies().toStack(ItemsRegistration.QUEEN.get()),
+                mutation.getSecondSpecies().toStack(ItemsRegistration.QUEEN.get()),
+                mutation.getResultSpecies().toStack(ItemsRegistration.QUEEN.get())
+        ));
     }
 
     @Override
@@ -75,19 +73,17 @@ public class MutationEmiRecipe implements EmiRecipe {
 
     @Override
     public List<EmiIngredient> getInputs() {
-        return List.of(first.get(0), second.get(1));
-    }
-
-    @Override
-    public List<EmiIngredient> getCatalysts() {
-        return catalysts;
+        return List.of(first, second);
     }
 
     @Override
     public List<EmiStack> getOutputs() {
-        ArrayList<EmiStack> emiStacks = new ArrayList<>(extraResults);
-        emiStacks.add(result.get(0));
-        return emiStacks;
+        return result.getEmiStacks();
+    }
+
+    @Override
+    public List<EmiIngredient> getCatalysts() {
+        return List.of(lookups);
     }
 
     @Override
@@ -103,13 +99,11 @@ public class MutationEmiRecipe implements EmiRecipe {
     @Override
     public void addWidgets(WidgetHolder widgets) {
         widgets.addTexture(ResourceLocation.tryBuild(MODID, "textures/gui/jei/mutations.png"), 0, 0, 143, 40, 0, 0, 143, 40, 143, 40);
-        // We use the same "unique" to ensure the slots stay synced. These ingredients may change once per interval.
-        // If we wanted to guarantee a change after each interval, we could make our own cycling slot class.
-        widgets.addGeneratedSlot(r -> getRandomIngredient(r, first), 1, 11, 11)
+        widgets.addSlot(first, 11, 11)
                 .drawBack(false);
-        widgets.addGeneratedSlot(r -> getRandomIngredient(r, second), 1, 58, 11)
+        widgets.addSlot(second, 58, 11)
                 .drawBack(false);
-        widgets.addGeneratedSlot(r -> getRandomIngredient(r, result), 2,114, 11)
+        widgets.addSlot(result, 114, 11)
                 .drawBack(false)
                 .recipeContext(this);
 
@@ -130,9 +124,5 @@ public class MutationEmiRecipe implements EmiRecipe {
             }
             widgets.addTooltipText(tips, 81, 1, 106 - 81, 10 - 1);
         }
-    }
-
-    private EmiIngredient getRandomIngredient(Random random, List<? extends EmiIngredient> ingredients) {
-        return ingredients.get(random.nextInt(ingredients.size()));
     }
 }
