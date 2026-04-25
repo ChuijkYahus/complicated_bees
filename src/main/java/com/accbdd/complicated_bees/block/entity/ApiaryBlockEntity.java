ec3582456b8491136f98c3d82ac03743d7146cde
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2026-04-25 00:39:01 -0700

    make queens check conditions as soon as frames change

--- src/main/java/com/accbdd/complicated_bees/block/entity/ApiaryBlockEntity.java
+++ src/main/java/com/accbdd/complicated_bees/block/entity/ApiaryBlockEntity.java
@@ -127,7 +127,7 @@ public class ApiaryBlockEntity extends BaseBeeHousing {
             @Override
             protected void onContentsChanged(int slot) {
                 setChanged();
-                getLogic().clearConditionCache();
+                getLogic().checkConditions();
             }
         };
     }
--- src/main/java/com/accbdd/complicated_bees/block/entity/mellarium/MellariumFrameHousingBlockEntity.java
+++ src/main/java/com/accbdd/complicated_bees/block/entity/mellarium/MellariumFrameHousingBlockEntity.java
@@ -31,6 +31,12 @@ public class MellariumFrameHousingBlockEntity extends AbstractMellariumBlockEnti
             public boolean isItemValid(int slot, @NotNull ItemStack stack) {
                 return stack.getItem() instanceof FrameItem;
             }
+
+            @Override
+            protected void onContentsChanged(int slot) {
+                getLogic().getController().ifPresent(controller -> controller.getLogic().checkConditions());
+                super.onContentsChanged(slot);
+            }
         };
         frameItemHandler = LazyOptional.of(() -> new AdaptedItemHandler(frameItems));
     }
