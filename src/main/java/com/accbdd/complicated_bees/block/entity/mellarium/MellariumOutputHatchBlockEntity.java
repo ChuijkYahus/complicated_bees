2f144ce7601c5ad55bbc6ff189797c21d31e6760
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2026-06-15 10:01:17 -0700

    clean code

--- src/main/java/com/accbdd/complicated_bees/block/entity/mellarium/MellariumOutputHatchBlockEntity.java
+++ src/main/java/com/accbdd/complicated_bees/block/entity/mellarium/MellariumOutputHatchBlockEntity.java
@@ -7,13 +7,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class MellariumOutputHatchBlockEntity extends AbstractMellariumBlockEntity implements IMellariumTickable {
     private LazyOptional<IItemHandlerModifiable> mellariumOutput;
--- src/main/java/com/accbdd/complicated_bees/multiblock/MellariumLogic.java
+++ src/main/java/com/accbdd/complicated_bees/multiblock/MellariumLogic.java
@@ -17,7 +17,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.items.IItemHandler;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class MellariumLogic {
     private final Level level;
